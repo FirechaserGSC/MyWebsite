0d4e83bea7f564b4f015b4ee34b126c7bee2473a
Shuchi <gsc820@gmail.com>
2017-09-06 23:38:10 -0500

    modify header

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-    res.send("test");
+    res.render("index");
 });
 
 app.listen("3000", function() {
