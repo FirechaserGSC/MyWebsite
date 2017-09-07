a7c802fe24de84976da6a89ca3fc6163c10275c6
Shuchi <gsc820@gmail.com>
2017-09-07 00:04:40 -0500

    add blog page

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
     res.render("index");
+});
+
+
+
+app.get("/blogs/new", function(req, res) {
+	res.render("newblog.ejs");
 });
 
 app.listen("3000", function() {
