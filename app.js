8ea12cc1a8558604240775d98e2c7e9ccc9fbf72
Shuchi <gsc820@gmail.com>
2017-09-11 23:31:54 -0500

    modify blogs page

--- app.js
+++ app.js
@@ -6,11 +6,24 @@ var mongoose = require("mongoose");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var blogs = [
+    {
+      title: "test1",
+      abstract: "test1 abstract"
+    },
+    {
+      title: "test2",
+      abstract: "test2 abstract"
+    }
+];
+
 app.get("/", function(req, res) {
     res.render("index");
 });
 
-
+app.get("/blogs", function(req, res) {
+    res.render("blogs", {blogs: blogs});
+})
 
 app.get("/blogs/new", function(req, res) {
 	res.render("newblog.ejs");
