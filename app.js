54c68f36ef36e9f9c2c7f705e72d2643ac228f1c
Shuchi <gsc820@gmail.com>
2017-09-12 00:34:38 -0500

    add blog show page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/blogs");
+mongoose.connect("mongodb://localhost/my_website");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -12,65 +12,46 @@ app.set("view engine", "ejs");
 var blogSchema = new mongoose.Schema({
     title: String,
     abstract: String,
+    body: String
 });
 
 var Blog = mongoose.model("Blog", blogSchema);
 
-// Blog.create(
-//   {
-//     title: "test1",
-//     abstract: "test1 abstract"
-//   }, function(err, blog) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log("blog created");
-//       console.log(blog);
-//     }
-//   });
-
-Blog.create(
-  {
-    title: "test2",
-    abstract: "test2 abstract"
-  }, function(err, blog) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("blog created");
-      console.log(blog);
-    }
-  });
-
-// var blogs = [
-//     {
-//       title: "test1",
-//       abstract: "test1 abstract"
-//     },
-//     {
-//       title: "test2",
-//       abstract: "test2 abstract"
-//     }
-// ];
-
 app.get("/", function(req, res) {
-    res.render("index");
+  res.render("index");
 });
 
 app.get("/blogs", function(req, res) {
-    // get blogs from db
-    Blog.find({}, function(err, blogs) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("blogs", {blogs, blogs});
-      }
-    });
+  Blog.find({}, function(err, blogs) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("blogs", {blogs: blogs});
+    }
+  });
 });
+
+app.post("/blogs", function(req, res) {
+  var title = req.body.title;
+  var abstract = req.body.abstract;
+  var newBlog = {title: title, abstract: abstract};
+
+  Blog.create(newBlog, function(err, newlyCreatedBlog) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/blogs");
+    }
+  })
+})
 
 app.get("/blogs/new", function(req, res) {
 	res.render("newblog.ejs");
 });
+
+app.get("blogs/:title", function(req, res) {
+  res.send("SHOW BLOG");
+})
 
 app.listen("3000", function() {
   console.log("server started");
