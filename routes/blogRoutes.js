58e994dbfdfcd16b83466c42d10b2e66c67d3269
Shuchi <gsc820@gmail.com>
2017-11-11 23:51:54 -0600

    add post blog route

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose');
 const Blog = mongoose.model('blogs');
 
 module.exports = app => {
+  app.post('/api/blogs', (req, res) => {});
+
   app.get('/api/blogs', async (req, res) => {
     const blogs = await Blog.find();
     res.send(blogs);
-    // const blogs = [{ title: 'test1' }, { title: 'test2' }];
-    // console.log(blogs);
-    // res.send(blogs);
   });
 };
